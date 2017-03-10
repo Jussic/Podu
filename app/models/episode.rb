443d1aea7797b96ebfe91843d5273c60775dcbe3
Justine Clarke <justineclarke_2017@depauw.edu>
2017-03-10 02:16:46 +0000

    Added image uploading to podcasts and episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,6 @@
 class Episode < ActiveRecord::Base
   belongs_to :podcast
+  
+  has_attached_file :episode_thumbnail, styles: { medium: "500x500#", large: "1000x1000#" } 
+  validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\z/
 end
